7c084c0b29357d765e1756d50522b941beb1c71f
Bruno Acosta <acostabruno35@gmail.com>
2025-08-14 21:46:04 -0300

    Validacion de campo Vacio

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 let amigos = [];
 
 
+
 function agregarAmigo(){
 let agregarNombres = document.getElementById('amigo').value
 console.log(agregarNombres)
+if (agregarNombres == ''){
+    alert('Por favor, escriba el nombre valido')
+}else{
+    amigos.push(agregarNombres)
+    console.log(amigos)
+}
 }
