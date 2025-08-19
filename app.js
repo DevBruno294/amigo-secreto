96d31c6e23a681e31e668f272419d78e18519162
Bruno Acosta <acostabruno35@gmail.com>
2025-08-18 21:34:03 -0300

    mostrar lista de amigos en pantalla

--- app.js
+++ app.js
@@ -4,16 +4,26 @@ let amigos = [];
 
 function agregarAmigo(){
 let agregarNombres = document.getElementById('amigo').value
-console.log(agregarNombres)
 if (agregarNombres == ''){
     alert('Por favor, escriba el nombre valido')
 }else{
     amigos.push(agregarNombres)
-    console.log(amigos)
     limpiarCaja()
+    mostrarLista(); 
 }
 }
 
 function limpiarCaja() {
     document.querySelector('#amigo').value = '';
+}
+
+function mostrarLista() {
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    amigos.forEach(function(nombre) {
+        const li = document.createElement('li');
+        li.textContent = nombre;
+        lista.appendChild(li);
+    });
 }
